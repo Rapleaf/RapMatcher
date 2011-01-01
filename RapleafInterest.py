290833316dce35941187c59e75f3f806f51fbf6f
Gareth G. Aye <gareth.aye@gmail.com>
2011-01-01 14:14:18 -0800

    Tune up

--- RapleafInterest.py
+++ RapleafInterest.py
@@ -1,13 +1,19 @@
 class RapleafInterest:
 	def __init__(self, name):
 		""" 
-		Takes a string and splits it by "&" to make Rapleaf segments thesaurus friendly
+		Splits by "&" to make Rapleaf segments thesaurus friendly
+		Takes phrases and appends constituent words
 		Replaces each word's spaces with underscores for WordNet formatting
 		"""
 		self.name = name
 		self.words = name.split(' & ')
-		for word in self.words:
-			word.replace(" ", "_")
+		for phrase in self.words:
+			parts = phrase.split()
+			if len(parts) > 1:
+				for word in parts:
+					self.words.append(word)
+		for i in range(len(self.words)):		
+			self.words[i] = self.words[i].replace(' ', '_')
 	
 	def get_words(self):
 		""" Returns an array of constituent words """
